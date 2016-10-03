60ca31395d79205ffaa9be4058b291e256d2eb15
wilmot_g <guillaume.wilmot@epitech.eu>
2016-10-03 23:42:25 +0200

    Prototyped the very next functions

--- pamela.cpp
+++ pamela.cpp
@@ -5,7 +5,7 @@
 ** Login   <wilmot_g@epitech.net>
 **
 ** Started on  Mon Oct 03 21:19:49 2016 wilmot_g
-** Last update Mon Oct 03 22:34:38 2016 wilmot_g
+** Last update Mon Oct 03 23:40:22 2016 wilmot_g
 */
 
 #include <security/pam_appl.h>
@@ -13,6 +13,27 @@
 #include <iostream>
 
 using namespace std;
+
+#define			PAM_SM_AUTH
+#define			PAM_SM_SESSION
+
+extern int		pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  if (!pamh)
+    return (PAM_SESSION_ERR);
+  return (PAM_SUCCESS);
+}
+
+extern int		pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (0);
+}
 
 int   main(int ac, char **av) {
   pam_handle_t  *pam;
