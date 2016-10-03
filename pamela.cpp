5be865b03f86f014f94286b38a2c4b3ba3808e6e
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-10-03 23:33:48 +0200

    Errors have been added + include file created to define error messages  properly

--- pamela.cpp
+++ pamela.cpp
@@ -5,12 +5,15 @@
 ** Login   <wilmot_g@epitech.net>
 **
 ** Started on  Mon Oct 03 21:19:49 2016 wilmot_g
-** Last update Mon Oct 03 22:34:38 2016 wilmot_g
+** Last update Mon Oct  3 23:31:45 2016 Nyrandone Noboud-Inpeng
 */
 
 #include <security/pam_appl.h>
-#include <security/pam_misc.h>
 #include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+// #include <security/pam_misc.h>
+#include "errors.hh"
 
 using namespace std;
 
@@ -21,8 +24,9 @@ int   main(int ac, char **av) {
   int           ret;
   int           retval;
 
-  if ((login = getlogin()) == NULL) return (0);
-  if ((ret = pam_start("vault", login, &conv, &pam)) == -1 || (ret = pam_authenticate(pam, 0)) == -1 || (ret = pam_acct_mgmt(pam, 0)) == -1);
+  if ((login = getlogin()) == NULL) return (cerr << ERR_LOGIN << endl, -1);
+  if ((ret = pam_start("vault", login, &conv, &pam)) == -1 || (ret = pam_authenticate(pam, 0)) == -1 || (ret = pam_acct_mgmt(pam, 0)) == -1)
+    return (cerr << pam_strerror(pam, retval) << endl, -1);
   cout << ret << endl;
   pam_end(pam, ret);
   return (0);
