2e92c7ed5ff93507756fed640a99e2093a5d4e7e
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-10-04 13:13:53 +0200

    Unused variables and defines added in Pamela.cpp and Pamela.hh

--- src/Pamela.cpp
+++ src/Pamela.cpp
@@ -5,7 +5,7 @@
 ** Login   <wilmot_g@epitech.net>
 **
 ** Started on  Mon Oct 03 21:19:49 2016 wilmot_g
-** Last update Mon Oct  3 23:31:45 2016 Nyrandone Noboud-Inpeng
+** Last update Tue Oct  4 13:12:40 2016 Nyrandone Noboud-Inpeng
 */
 
 #include <security/pam_appl.h>
@@ -14,40 +14,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "Errors.hh"
+#include "Pamela.hh"
 
 using namespace std;
 
 #define			PAM_SM_AUTH
 #define			PAM_SM_SESSION
 
-extern int		pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+extern int		pam_sm_open_session(UNUSED pam_handle_t *pamh, UNUSED int flags,
+                                  UNUSED int argc, UNUSED const char **argv)
 {
-  (void)pamh;
-  (void)flags;
-  (void)argc;
-  (void)argv;
   if (!pamh)
     return (PAM_SESSION_ERR);
   return (PAM_SUCCESS);
 }
 
-extern int		pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+extern int		pam_sm_close_session(UNUSED pam_handle_t *pamh, UNUSED int flags,
+                                    UNUSED int argc, UNUSED const char **argv)
 {
-  (void)pamh;
-  (void)flags;
-  (void)argc;
-  (void)argv;
   return (0);
 }
 
-int			main(int ac, char **av) {
+int			main(UNUSED int ac, UNUSED char **av) {
   pam_handle_t		*pamh;
   pam_conv		conv;
   char			*login;
   int			ret;
 
-  (void)ac;
-  (void)av;
   if (!(login = getlogin())) return (cerr << ERR_LOGIN << endl, -1);
   if ((ret = pam_start("vault", login, &conv, &pamh)) == -1 || (ret = pam_authenticate(pamh, 0)) == -1 || (ret = pam_acct_mgmt(pamh, 0)) == -1)
     return (cerr << pam_strerror(pamh, ret) << endl, pam_end(pamh, ret), -1);
